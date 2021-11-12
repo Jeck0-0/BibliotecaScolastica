a61f40d1414a2cbc64b19015b02526c1c47e4f86
Jeck0_0 <jrubboli@gmail.com>
2021-11-12 09:14:30 +0100

    biblioteca_v0.1 variabili e metodi non implementati

---
+++ Biblioteca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaScolastica
+{
+    public class Biblioteca
+    {
+        string _nome;
+        string _indirizzo;
+        string _orarioApertura;
+        string _orarioChiusura;
+
+        List<Libro> _libri;
+
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+        }
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura, List<Libro> libri) : this(nome,indirizzo, orarioApertura, orarioChiusura)
+        {
+        }
+
+        public int NumeroLibri() => Libri.Count;
+
+        public List<Libro> LibriDellAutore(string nomeAutore)
+        {
+        }
+
+        public List<Libro> CercaLibriConTitolo(string titolo)
+        {
+        }
+
+        public void AggiungiLibro(Libro libro)
+        {
+        }
+    }
+}
