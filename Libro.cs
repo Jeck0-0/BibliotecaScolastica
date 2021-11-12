c974541ef66f79a61f2b7bb41135189ced3a6266
Jeck0_0 <jrubboli@gmail.com>
2021-11-12 08:40:57 +0100

    libro_v0.2 variabili e metodi implementati

--- Libro.cs
+++ Libro.cs
@@ -8,25 +8,67 @@ namespace BibliotecaScolastica
     {
         string _autore;
         string _titolo;
-        string _annoPubblicazione;
         string _editore;
+        int _annoPubblicazione;
         int _nPagine;
 
-        public Libro()
+        public string Autore { 
+            get { return _autore; } 
+            set { _autore = value; } 
+        }
+        public string Titolo
         {
+            get { return _titolo; }
+            set { _titolo = value; }
+        }
+        public string Editore
+        {
+            get { return _editore; }
+            set { _editore = value; }
+        }
+        public int AnnoPubblicazione
+        {
+            get { return _annoPubblicazione; }
+            set {
+                if (value < DateTime.Now.Year)
+                    _annoPubblicazione = value;
+                else
+                    throw new Exception("anno non valido");
+            }
+        }
+        public int NPagine
+        {
+            get { return _nPagine; }
+            set { 
+                if (value > 0) _nPagine = value; 
+                else throw new Exception("non possono esserci meno di 0 pagine"); 
+            }
+        }
 
+        public Libro(string autore, string titolo, string editore, int anno, int nPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            Editore = editore;
+            AnnoPubblicazione = anno;
+            NPagine = nPagine;
         }
 
 
-        public int ReadingTime()
+        public string ReadingTime()
         {
-            return 0;
+            if (NPagine < 100)
+                return "1h";
+            else if (NPagine > 200)
+                return "2h+";
+            else 
+                return "2h";
         }
 
 
         public override string ToString()
         {
-            return base.ToString();
+            return $"[{AnnoPubblicazione}] {Titolo} (di {Autore}, editore: {Editore}) - {NPagine} pagine";
         }
     }
 }
