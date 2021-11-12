436149c7c4ed83336441d425c37b8ffdc5bd22e4
Jeck0_0 <jrubboli@gmail.com>
2021-11-12 08:29:17 +0100

    libro_v0.1 variabili essenziali non implementate

---
+++ Libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaScolastica
+{
+    class Libro
+    {
+        string _autore;
+        string _titolo;
+        string _annoPubblicazione;
+        string _editore;
+        int _nPagine;
+
+        public Libro()
+        {
+
+        }
+
+
+        public int ReadingTime()
+        {
+            return 0;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
